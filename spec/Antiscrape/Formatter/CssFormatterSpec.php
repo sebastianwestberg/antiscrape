5a63a6951d4251a43fe98fb1f7aafc91dead4ba4
Sebastian Westberg <sebastian@stormfors.se>
2015-04-16 21:21:49 +0200

    Fixed indentations

--- spec/Antiscrape/Formatter/CssFormatterSpec.php
+++ spec/Antiscrape/Formatter/CssFormatterSpec.php
@@ -15,21 +15,21 @@ class CssFormatterSpec extends ObjectBehavior
 
     function it_generates_a_random_css_class()
     {
-    	$nameLen = 15;
-    	$this->nameGenerator($nameLen)->shouldBeValidCssClassName($nameLen);
+        $nameLen = 15;
+        $this->nameGenerator($nameLen)->shouldBeValidCssClassName($nameLen);
     }
 
-	function getMatchers()
-	{
+    function getMatchers()
+    {
         return [
             'beValidStyle' => function($subject) {
-            	$pattern = '/^(\.[a-z]*{display: ?(none|inline)})+$/i';
+                $pattern = '/^(\.[a-z]*{display: ?(none|inline)})+$/i';
                 return preg_match($pattern, $subject);
             },
             'beValidCssClassName' => function($subject, $chars = 8) {
-            	$pattern = sprintf('/^[a-zA-Z]{%d}$/', $chars);
-            	return preg_match($pattern, $subject);
+                $pattern = sprintf('/^[a-zA-Z]{%d}$/', $chars);
+                return preg_match($pattern, $subject);
             }
         ];
-	}
+    }
 }
--- src/Antiscrape/Formatter/CssFormatter.php
+++ src/Antiscrape/Formatter/CssFormatter.php
@@ -4,34 +4,34 @@ namespace Antiscrape\Formatter;
 
 class CssFormatter implements FormatterInterface
 {
-	private $css = array();
+    private $css = array();
 
-	public function addFormatting($data = array())
-	{
-		$this->css[] = sprintf(".%s{%s}", $data['selector'], $data['style']);
-	}
+    public function addFormatting($data = array())
+    {
+        $this->css[] = sprintf(".%s{%s}", $data['selector'], $data['style']);
+    }
 
-	public function getFormatting()
-	{
-		$str = '';
+    public function getFormatting()
+    {
+        $str = '';
 
-		foreach ($this->css as $css) {
-			$str .= $css;
-		}
+        foreach ($this->css as $css) {
+            $str .= $css;
+        }
 
-		return $str;
-	}
+        return $str;
+    }
 
-	public function nameGenerator($classLen = 8, $chars = 'abcdefghijklmnopqrstuvwxyz')
-	{
-		$charsLen = strlen($chars) - 1;
-		$class = '';
+    public function nameGenerator($classLen = 8, $chars = 'abcdefghijklmnopqrstuvwxyz')
+    {
+        $charsLen = strlen($chars) - 1;
+        $class = '';
 
-		for ($i = 0; $i < $classLen; $i++) {
-			$randEntry = mt_rand(1, $charsLen);
-			$class .= $randEntry % 2 === 0 ? $chars[$randEntry] : ucfirst($chars[$randEntry]);
-		}
+        for ($i = 0; $i < $classLen; $i++) {
+            $randEntry = mt_rand(1, $charsLen);
+            $class .= $randEntry % 2 === 0 ? $chars[$randEntry] : ucfirst($chars[$randEntry]);
+        }
 
-		return $class;
-	}
+        return $class;
+    }
 }
